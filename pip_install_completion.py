92372d78e03ca734a155856994aefb3cc5fd8088
dzikie drożdże <daz@hackerspace.pl>
2015-12-14 15:17:20 +0100

    six, logging, performance update

--- pip_install_completion.py
+++ pip_install_completion.py
@@ -4,44 +4,57 @@ import time
 import sqlite3
 from itertools import cycle
 from collections import defaultdict
+import six.moves.xmlrpc_client as xmlrpclib
+import logging
 
 from pip.commands import SearchCommand
 
-try:
-    import xmlrpclib
-except ImportError:
-    import xmlrpc.client as xmlrpclib
 
-PIP_SEARCH_DB_FILENAME = 'pip_search.db'
+logger = logging.Logger('pip', level=logging.NOTSET)
+
+PIP_SEARCH_DB_FILENAME = '{pip_dir}/pip_search.db'
 INDEX_CHECK_TIMEOUT = 10 * 60
 
 SCHEMA = """
-    CREATE TABLE IF NOT EXISTS indexes (
+    CREATE TABLE indexes (
         id INTEGER PRIMARY KEY,
         timestamp INTEGER NOT NULL,
         url TEXT NOT NULL UNIQUE
     );
 
-    CREATE TABLE IF NOT EXISTS packages (
+    CREATE TABLE packages (
         index_id INTEGER NOT NULL,
         name TEXT NOT NULL,
-        FOREIGN KEY (index_id) REFERENCES indexes(id)
+        FOREIGN KEY (index_id) REFERENCES indexes(id),
+        PRIMARY KEY (index_id, name)
     );
 """
 
 
 class VeryORM:
     def __init__(self, db_path, pip_index):
+        logger.debug("new db {0}, {1}".format(db_path, pip_index))
         self.db_path = db_path
         self.init_db()
         self.index = pip_index
-        if pip_index not in self.indexes: self.create_index(pip_index) # noqa
+        if pip_index not in self.indexes:
+            self.create_index(pip_index)
+
         self.index_id = self.get_index_id(pip_index)
+        logger.debug("index id: {}".format(self.index_id))
 
     def init_db(self):
         self.conn = sqlite3.connect(self.db_path)
         self.conn.row_factory = sqlite3.Row
-        self.conn.executescript(SCHEMA)
+        try:
+            self.conn.executescript(SCHEMA)
+            created = True
+        except sqlite3.OperationalError as exc:
+            if 'table indexes already exists' in exc.args:
+                created = False
+            else:
+                raise
+        return created
 
     def create_index(self, index):
         q = "INSERT INTO indexes(timestamp, url) VALUES(?, ?)"
@@ -70,6 +83,11 @@ class VeryORM:
         id = self.index_id
         q = 'INSERT INTO packages (index_id, name) VALUES(?, ?)'
         self.conn.execute(q, [id, package])
+
+    def add_packages(self, package_names):
+        q = 'INSERT OR IGNORE INTO packages (index_id, name) VALUES(?, ?)'
+        id = self.index_id
+        self.conn.executemany(q, zip(cycle([id]), package_names))
 
     def rem_package(self, package):
         if not self.has_package(package):
@@ -104,25 +122,37 @@ class VeryORM:
         result = self.conn.execute(q, [index]).fetchone()
         return result['id'] if result else None
 
+    def get_index_urls(self):
+        q = 'SELECT url FROM indexes'
+        result = self.conn.execute(q).fetchall()
+        return [idx['url'] for idx in result]
+
     @property
     def indexes(self):
-        def contains(_, index): return self.get_index_id(index) is not None # noqa
-        return type("", (), {'__contains__': contains})()
+        return self.get_index_urls()
 
 
 class SearchCache:
 
-    def __init__(self, index, db_path):
-        self.client = xmlrpclib.ServerProxy(index)
-        self.db = VeryORM(db_path, index)
+    def __init__(self, db_path, index=None):
+        defaults = SearchCommand().parse_args([])[0]
+        self.index = index or defaults.index
+        self.db_path = db_path.format(pip_dir=defaults.cache_dir)
+
+        self.client = xmlrpclib.ServerProxy(self.index)
+        self.db = VeryORM(self.db_path, self.index)
 
     def search(self, name):
-        last_update_ts = self.db.get_timestamp()
-        now = int(time.time())
-        timeout_gone = last_update_ts + INDEX_CHECK_TIMEOUT < now
-        if timeout_gone:
+        logger.info("searching for {}".format(name))
+        if self.database_stale:
             self.update()
         return self.db.get_packages(name)
+
+    @property
+    def database_stale(self):
+        last_update_ts = self.db.get_timestamp()
+        now = int(time.time())
+        return last_update_ts + INDEX_CHECK_TIMEOUT < now
 
     def get_changes_since(self, timestamp):
         changes_dict = defaultdict(set)
@@ -134,34 +164,29 @@ class SearchCache:
         return changes_dict
 
     def update(self):
+        logger.info("updating!")
         last_ts = self.db.get_timestamp()
-        if last_ts == 0:
-            packages = self.client.list_packages()
-            self.db.set_packages(packages)
-            return
+        if last_ts == 0:  # not initialized
+            self._fetch_all()
+        else:
+            self._fetch_since(last_ts)
 
-        changes = self.get_changes_since(last_ts)
-
-        for package in changes.get('remove', []):
-            self.db.rem_package(package)
-        for package in changes.get('create', []):
-            self.db.add_package(package)
         self.db.set_timestamp(time.time())
 
+    def _fetch_since(self, ts):
+        changes = self.get_changes_since(ts)
+        self.db.add_packages(changes.get('create', []))
 
-def cached_search(query):
-    sc = SearchCommand()
-    options, _ = sc.parse_args([])
-    index = options.index
-    db_path = os.path.join(options.cache_dir, PIP_SEARCH_DB_FILENAME)
-    cache = SearchCache(index, db_path)
-    return cache.search(query)
+    def _fetch_all(self):
+        logger.info("not initialized yet")
+        packages = self.client.list_packages()
+        self.db.set_packages(packages)
 
 
 def main():
+    cache = SearchCache(PIP_SEARCH_DB_FILENAME)
     query = sys.argv[1] if len(sys.argv) > 1 else ''
-    pkgnames = cached_search(query)
-    print('\n'.join(pkgnames))
+    print('\n'.join(cache.search(query)))
 
 
 if __name__ == "__main__":
